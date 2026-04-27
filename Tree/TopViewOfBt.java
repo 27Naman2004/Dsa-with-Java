6cb7b2cb3bc47ca7fdb72bc37a8db15b97e045f7
Naman <naman.12323443@lpu.in>
2026-04-27 19:56:38 +0530

    Learning Continue

--- Tree/TopViewOfBt.java
+++ Tree/TopViewOfBt.java
@@ -1,7 +1,8 @@
-import java.util.*;
+
+
 public class TopViewOfBt {
-    p
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
     }
 }
--- Tree/VerticalOrderTraversal.java
+++ Tree/VerticalOrderTraversal.java
@@ -1,51 +1,106 @@
+
 import java.util.*;
+
 public class VerticalOrderTraversal {
-    class Node{
+
+    class Node {
+
         Node left;
         Node right;
         int val;
-        Node(int val){
+
+        Node(int val) {
             this.val = val;
             right = left = null;
         }
     }
-    static class Pair{
+
+    static class Pair {
+
         Node node;
         int hd;
-        Pair(Node node,int hd){
+        int level;
+        Pair(Node node, int hd, int level) {
             this.node = node;
             this.hd = hd;
+            this.level = level;
         }
     }
-    public static List<List<Integer>> VerticalOrder(Node root){
+
+    public static List<List<Integer>> VerticalOrder(Node root) {
         List<List<Integer>> ans = new ArrayList<>();
-        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(root,0));
         if(root==null) return ans;
-        while(!q.isEmpty()){
-            Pair curr = q.poll();
-            Node node = curr.node;
-            int hd = curr.hd;
-
-            if(!map.containsKey(hd)){
-                map.put(hd, new ArrayList<>());
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0, 0));
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+            Node node = p.node;
+            int hd = p.hd;
+            int level = p.level;
+            if(!map.containsKey(hd)) {
+                map.put(hd, new TreeMap<>());
             }
-            map.get(hd).add(node.val);
-
-            if(node.left!=null){
-                q.offer(new Pair(node, hd-1));
+            if(!map.get(hd).containsKey(level)) {
+                map.get(hd).put(level, new PriorityQueue<>());
             }
-            if(node.right!=null){
-                q.offer(new Pair(node, hd+1));
+            map.get(hd).get(level).offer(node.val);
+            if(node.left!=null) {
+                q.offer(new Pair(node.left, hd-1, level+1));
+            }
+            if(node.right!=null) {
+                q.offer(new Pair(node.right, hd+1, level+1));
             }
         }
-        for(ArrayList<Integer> list : map.values()){
-            ans.add(list);
+        for(TreeMap<Integer, PriorityQueue<Integer>> tm : map.values()) {
+            ans.add(new ArrayList<>());
+            for(PriorityQueue<Integer> pq : tm.values()) {
+                while(!pq.isEmpty()) {
+                    ans.get(ans.size()-1).add(pq.poll());
+                }
+            }
         }
         return ans;
     }
-    public static void main(String[] args){
+        
+        //more com-pact version
+
+    public static List<List<Integer>> VerticalOrderTraversal(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0, 0));
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+            Node node = p.node;
+            int hd = p.hd;
+            int level = p.level;
+            map.putIfAbsent(hd, new TreeMap<>());
+            
+            map.get(hd).putIfAbsent(level, new PriorityQueue<>());
+            map.get(hd).get(level).offer(node.val);
+
+            if(node.left!=null) {
+                q.offer(new Pair(node.left, hd-1, level+1));
+            }
+            if(node.right!=null) {
+                q.offer(new Pair(node.right, hd+1, level+1));
+            }
+        }
+        for(var tm: map.values()) {
+            ans.add(new ArrayList<>());
+            for(var pq : tm.values()) {
+                while(!pq.isEmpty()) {
+                    ans.get(ans.size()-1).add(pq.poll());
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
 
     }
 }
