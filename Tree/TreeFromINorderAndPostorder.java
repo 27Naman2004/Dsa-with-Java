53f1514a4efeaeac490ed31e71c8aff7004d2b66
Naman <naman.12323443@lpu.in>
2026-05-07 13:22:28 +0530

    Trees Continues

---
+++ Tree/TreeFromINorderAndPostorder.java
@@ -0,0 +1,22 @@
+public class TreeFromINorderAndPostorder {
+
+
+    class Node {
+
+        Node left;
+        Node right;
+        int val;
+
+        Node(int val) {
+            this.val = val;
+            right = left = null;
+        }
+    }
+
+    
+
+
+    public static void main(String[] args) {
+
+    }
+}
